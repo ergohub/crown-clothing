5db12d8ba3a288481095113aad664e107c04252e
Derek Stanley <ergohub@icloud.com>
2025-05-20 13:29:51 +0100

    Can move on to signup with redirect

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.util';
+import { signInWithGooglePopup, createUserFromAuth, } from '../../utils/firebase/firebase.util';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserFromAuth(user)
     }
 
     return (
--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -5,6 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Import Firebase Auth libraries
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -26,3 +27,26 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid); // database, collection, unique id identifier
+
+    const UserSnapShot = await getDoc(userDocRef)
+
+    console.log(UserSnapShot);
+
+    if (!UserSnapShot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt }
+            )
+        } catch (error) {
+            console.log('Error creating user', error.message);
+        }
+    }
+    return userDocRef;
+
+}
